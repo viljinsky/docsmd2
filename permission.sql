bd83d3e066bb4c3b2007da5bb100b6e11139331a
viljinsky <viljinsky@narod.ru>
2016-04-15 08:27:08 +0300

    15 04 2016 docsmd2

--- permission.sql
+++ permission.sql
@@ -1,5 +1,9 @@
 use docsmd;
 
+--   Права пользователя 14 апр 2016
+
+drop trigger if exists users_after_insert;
+
 drop table if exists users_permission;
 drop table if exists permission;
 
@@ -12,8 +16,8 @@ insert into permission values
  (1,'add_message','Добавлять сообщения'),
  (2,'replay_message','Отвечать на сообщения'),
  (3,'add_attachment','Прикреплять файлы') ;
-
-select * from permission;   
+ 
+ 
 
 create table users_permission(
 user_id integer not null, 
@@ -23,8 +27,13 @@ constraint fk_users_permission foreign key (user_id) references users(user_id) o
 constraint fk_users_premission_permission foreign key (permission_id) references permission(permission_id),
 constraint uq_users_permission unique(user_id,permission_id)
 );
+-- После доавления пользователя - нужно назначить права !
+create trigger users_after_insert after insert on users for each  row
+ insert into users_permission (user_id,permission_id) select new.user_id,permission_id from permission; 
 
-
+-- есть прописать права
 insert into users_permission (user_id,permission_id) select user_id,permission_id from users,permission;
 
+-- посмотреть что получилось
+select * from permission;   
 select * from users_permission;
